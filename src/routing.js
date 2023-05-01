98147e3db6fd0f0193f4f4a9b7b356b561d38ac6
rain72510 <82126940+rain72510@users.noreply.github.com>
2023-05-02 02:02:59 +0800

    Add files via upload

---
+++ src/routing.js
@@ -0,0 +1,11 @@
+const routes = {
+  home: "/",
+  products: "/products",
+  // character: "/characters/:id",
+  notImplemented: "/notImplemented",
+  // movie: "/movies/:id"
+  // details: "/products",
+  cart: "/shoppingCart"
+};
+
+export default routes;
